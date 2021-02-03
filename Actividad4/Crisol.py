b371d8cdccc0e7f8f99e0b13b5a02eade72deba8
Fatake <pace_lozano@hotmail.com>
2021-02-02 18:13:30 -0600

    Cambiando Programa de Crisol

--- Actividad4/Crisol.py
+++ Actividad4/Crisol.py
@@ -8,34 +8,40 @@ def aplicarGas(Gas):
 def main():
     memoria = open('datos.dat','w+')
 
-    temp = 0
-    Gas = 4
-    Vent = 0
+    temp = 0 # maximo hasta 320
+    Gas = 0 # 4 representa un 4 %
+    Vent = 0 # 1 prendido 0 Apagado
+
+    # Cuando vent = 1 entonces Gas se baja al 0%
 
     memoria.write("Temp    Gas\n")
     print("<-- Iniciando Programa controlador Cristol -->")
-    while (True):
+    
+    for i in range(300):
         # Sensar
         temp = sensaTemperatura(temp, Gas, Vent)
+        
         print("[i] Temperatura: {0} Gas: {1}".format(temp,Gas))
 
-        str = "{0}    {1}\n".format(temp,Gas)
-
         # Registra
+        str = "{0}    {1}\n".format(temp,Gas)
         memoria.write(str)
 
+        # Pregunta sobre la temperatura
         if( temp > 320):
-            Vent += 1
+            Vent = 1
             if(Gas != 0):
-                print( "[-] Bajando nivel de gas") 
-                Gas -= 1
-            print ("[+] Aumentando ventilacion")
+                Gas = 0
+                print( "[-] Gas al 0% ") 
+
+            print ("[+] Ventilacion prendida")
         else:
             if(Vent != 0):
                 Vent = 0
-                print ("[-] Apagando ventilacion")
+                print ("[-] Ventilaci�n Apagada")
+                
             print( "[+] Aumentando nivel de gas") 
-            Gas += 1
+            Gas += 10
             
             # Actua
             aplicarGas(Gas)
