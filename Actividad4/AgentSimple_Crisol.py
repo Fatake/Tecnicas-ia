93e13af5f8a71ca330c6ae495dde84947812e070
ggzor <30713864+ggzor@users.noreply.github.com>
2021-02-05 16:46:59 -0600

    Cambiando open y close por with

--- Actividad4/AgentSimple_Crisol.py
+++ Actividad4/AgentSimple_Crisol.py
@@ -6,48 +6,45 @@ def aplicarGas(Gas):
     print("[+] Aplicando {0}% de Gas".format(Gas))
 
 def main():
-    memoria = open('datos.dat','w+')
-
     temp = 0 # maximo hasta 320
     Gas = 0 # 4 representa un 4 %
     Vent = 0 # 1 prendido 0 Apagado
 
     # Cuando vent = 1 entonces Gas se baja al 0%
 
-    memoria.write("Temp,Gas\n")
-    print("<-- Agente Simple Cristol -->")
-    
-    for i in range(300):
-        # Sensar
-        temp = sensaTemperatura(temp, Gas, Vent)
+    with open('datos.dat','w+') as memoria:
+        memoria.write("Temp,Gas\n")
+        print("<-- Agente Simple Cristol -->")
         
-        print("[i] Temperatura: {0} Gas: {1}".format(temp,Gas))
-
-        # Registra
-        str = "{0}    {1}\n".format(temp,Gas)
-        memoria.write(str)
-
-        # Pregunta sobre la temperatura
-        if( temp > 320):
-            Vent = 1
-            if(Gas != 0):
-                Gas = 0
-                print( "[-] Gas al 0% ") 
-
-            print ("[+] Ventilacion prendida")
-        else:
-            if(Vent != 0):
-                Vent = 0
-                print ("[-] Ventilacion Apagada")
-                
-            print( "[+] Aumentando nivel de gas") 
-            Gas += 10
+        for i in range(300):
+            # Sensar
+            temp = sensaTemperatura(temp, Gas, Vent)
             
-            # Actua
-            aplicarGas(Gas)
-        print("\n")
+            print("[i] Temperatura: {0} Gas: {1}".format(temp,Gas))
 
-    memoria.close()
+            # Registra
+            str = "{0}    {1}\n".format(temp,Gas)
+            memoria.write(str)
+
+            # Pregunta sobre la temperatura
+            if( temp > 320):
+                Vent = 1
+                if(Gas != 0):
+                    Gas = 0
+                    print( "[-] Gas al 0% ") 
+
+                print ("[+] Ventilacion prendida")
+            else:
+                if(Vent != 0):
+                    Vent = 0
+                    print ("[-] Ventilacion Apagada")
+                    
+                print( "[+] Aumentando nivel de gas") 
+                Gas += 10
+                
+                # Actua
+                aplicarGas(Gas)
+            print("\n")
 
 if __name__ == "__main__":
     main()
