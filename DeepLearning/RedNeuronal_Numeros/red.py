3c3a6c99d96ff0f9f85ffd732b8758ceccee3e73
Fatake <pace_lozano@hotmail.com>
2021-05-19 09:21:39 -0500

    Terminando red neuronal de numeros

--- DeepLearning/RedNeuronal_Numeros/red.py
+++ DeepLearning/RedNeuronal_Numeros/red.py
@@ -7,10 +7,29 @@ import matplotlib.pyplot as plt
 (train_data, train_labels), (test_data, test_labels) = mnist.load_data()
 
 print(train_data.shape)
-
+print("mostrando el numero {0}".format(train_labels[45]) )
 plt.imshow(train_data[45]) # Muestra un 9
-plt.show
+plt.show()
 
 model = models.Sequential()
 model.add(layers.Dense(512,activation='relu', input_shape=(28*28,)))
 model.add(layers.Dense(10,activation='softmax'))
+
+model.compile(optimizer='rmsprop',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+# Limpieza de datos
+x_train = train_data.reshape((60000,28*28))
+x_train = x_train.astype('float32')/255
+
+x_test = test_data.reshape((10000,28*28))
+x_test = x_test.astype('float32')/255   
+
+y_train = to_categorical(train_labels)
+y_test = to_categorical(test_labels)
+
+## Entrenamiento
+model.fit(x_train, y_train, epochs=5, batch_size=128)
+
+# Evaluacion model.evaluate(x_test,y_test)
